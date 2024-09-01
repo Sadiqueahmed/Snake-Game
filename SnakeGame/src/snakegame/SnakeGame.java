f0e170ef4529e58c0a633b33a6cd850fdd7cbe3a
Sadique Ahmed <140674441+Sadiqueahmed@users.noreply.github.com>
2024-09-01 07:25:30 -0700

    Add files via upload

--- SnakeGame/src/snakegame/SnakeGame.java
+++ SnakeGame/src/snakegame/SnakeGame.java
@@ -1,31 +1,19 @@
-
 package snakegame;
 
 import javax.swing.*;
 
-/**
- *
- * @author sadiq
- */
 public class SnakeGame extends JFrame {
     
-    SnakeGame(){
+    SnakeGame() {
         super("Snake Game");
-        add(new Board()); 
+        add(new Board());
         pack();
         
-        
-        setSize(300,300);
         setLocationRelativeTo(null);
-        setVisible(true);
+        setResizable(false);
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        new SnakeGame();
+        new SnakeGame().setVisible(true);
     }
-    
 }
